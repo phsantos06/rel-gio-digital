2954a7773308acd96b089c7e5d019ac262665eb5
phsantos06 <ph294420@gmail.com>
2026-02-18 01:28:47 -0300

    Add files via upload

---
+++ relogio.js
@@ -0,0 +1,13 @@
+function atualizarRelogio() {
+    const agora = new Date();
+
+    let horas = agora.getHours().toString().padStart(2, "0");
+    let minutos = agora.getMinutes().toString().padStart(2, "0");
+    let segundos = agora.getSeconds().toString().padStart(2, "0");
+
+    document.getElementById("relogio").textContent =
+    `${horas}:${minutos}:${segundos}`;
+}
+
+setInterval(atualizarRelogio, 1000);
+atualizarRelogio();
